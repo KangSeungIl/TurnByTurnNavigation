d493f6ef2e515b2ba3d393d33699f0afe12addba
kangsi <tmddlf51@naver.com>
2017-02-13 16:49:46 +0900

    AutoComplete 리뉴얼 완료

--- app/src/main/java/lbsproject/turnbyturnnavi/MapsActivity.java
+++ app/src/main/java/lbsproject/turnbyturnnavi/MapsActivity.java
@@ -12,10 +12,10 @@ import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import android.util.Log;
 
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -28,14 +28,16 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import static lbsproject.turnbyturnnavi.R.id.map;
 
-public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
     private GoogleMap mMap;
+    private LocationManager manager;
+    private GPSListener gpsListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +47,17 @@ public class MapsActivity extends FragmentActivity
                 .findFragmentById(map);
         mapFragment.getMapAsync(this);
 
+        // 위치 관리자 객체 참조
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // 위치 정보를 받을 리스너 생성
+        gpsListener = new GPSListener();
+
         // autocomplete activity 실행할 Button
         Button openButton = (Button) findViewById(R.id.open_button);
         openButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                stopMyLocationService(); // 현재 위치로 반환하는 위치 정보 수집 종료
                 openAutocompleteActivity(); // autocomplete를 실행할 Activity 실행
             }
         });
@@ -65,23 +73,22 @@ public class MapsActivity extends FragmentActivity
         // Permission 설정 확인 (Runtime)
         if (ActivityCompat.checkSelfPermission
                 (this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission
-                (this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         // Device의 위치 표시 (마커)
         mMap.setMyLocationEnabled(true);
-        // Device의 현재 위치를 반환해주는 버튼을 삭제
-        mMap.getUiSettings().setMyLocationButtonEnabled(false);
     }
 
+    /**
+     * AutoComplete를 실행해줄 Activity에 대한 함수
+     */
     private void openAutocompleteActivity() {
         try {
             // Intent를 이용하여 Google play Services가 가능 한지 판단
             Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
                     .build(this);
+            // onActivityResult함수로 값을 넘겨줌
             startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
         } catch (GooglePlayServicesRepairableException e) {
             GoogleApiAvailability.getInstance().getErrorDialog(this, e.getConnectionStatusCode(),
@@ -89,12 +96,17 @@ public class MapsActivity extends FragmentActivity
         } catch (GooglePlayServicesNotAvailableException e) {
             String message = "Google Play Services is not available: " +
                     GoogleApiAvailability.getInstance().getErrorString(e.errorCode);
+            Log.e("Error", message);
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         }
     }
 
     /**
      * autocomplete activity 끝나고 나서 결과를 불러내는 함수
+     *
+     * @param requestCode AutoCompleteActivity에서 넘겨주는 requestCode
+     * @param resultCode AutoCompleteActivity에서 넘겨주는 resultCode
+     * @param data AutoCompleteActivity에서 넘겨주는 Intent
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -103,21 +115,30 @@ public class MapsActivity extends FragmentActivity
         // autocomplete widget으로 부터의 결과를 체크
         if (requestCode == REQUEST_CODE_AUTOCOMPLETE) {
             if (resultCode == RESULT_OK) {
+                // 선택한 장소에 대한 데이터 가져옴
                 Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i("Place Selected: ", "" + place.getName());
+
+                // 선택한 장소로 Camera 이동
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 15));
+                // MapType은 Normal로 설정
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                // 선택한 장소에 마커 표시
+                mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title((String)place.getName()));
 
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                // 결과 에러가 났을 경우 상태 데이터를 가져옴
                 Status status = PlaceAutocomplete.getStatus(this, data);
+                Log.e("Error: Status = ", "" + status.toString());
             } else if (resultCode == RESULT_CANCELED) {
             }
         }
     }
 
-    private void startLocationService() {
-        // 위치 관리자 객체 참조
-        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        // 위치 정보를 받을 리스너 생성
-        GPSListener gpsListener = new GPSListener();
+    /**
+     * 현재 위치를 반환해주기 위한 함수
+     */
+    private void startMyLocationService() {
         long minTime = 10000; // 10초 단위로 Reset
         float minDistance = 0; // 거리는 0단위로 Reset
 
@@ -141,7 +162,19 @@ public class MapsActivity extends FragmentActivity
     }
 
     /**
-     * 리스너 클래스 정의
+     * 현재 위치를 반환을 중지해주기 위한 함수
+     */
+    private void stopMyLocationService() {
+        try {
+            // 위치 정보 수집 종료
+            manager.removeUpdates(gpsListener);
+        } catch(SecurityException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * 현재위치에 대한 LocationListener를 GPSListener 클래스로 정의
      */
     private class GPSListener implements LocationListener {
         /**
@@ -184,7 +217,6 @@ public class MapsActivity extends FragmentActivity
 
     // GPS Setting 상태 확인
     private void enableGPSSetting() {
-        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         boolean gpsEnabled = manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
         // 초기 GPS가 꺼져있다면 GPS 설정으로 넘어 갈 수 있도록 함
         if (!gpsEnabled) {
@@ -196,7 +228,7 @@ public class MapsActivity extends FragmentActivity
                         public void onClick(DialogInterface dialog, int which) {
                             Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                             startActivity(intent);
-                            startLocationService();
+                            startMyLocationService();
                         }
                     })
                     // 닫기를 Click하면 초기 화면인 Sdney에서 변경 없음
@@ -205,8 +237,9 @@ public class MapsActivity extends FragmentActivity
                         public void onClick(DialogInterface dialog, int which) {
                         }
                     }).show();
-        } else { // GPS가 켜져 있다면 바로 현재 위치로
-            startLocationService();
+        } else {
+            // GPS가 켜져 있다면 바로 현재 위치로
+            startMyLocationService();
         }
     }
 }
